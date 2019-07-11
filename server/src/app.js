abff07f29540c1e4e3caefef40b6fc6c5500b6ec
Niklas <nkarow@gmx.de>
2019-07-11 17:19:41 +0200

    added express to server, get on server /status works with example, serve script for server created

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,22 @@
-console.log("Test");
+console.log("Test 123");
+
+
+const express = require('express')
+const bodyParser = require('body-Parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'hello world'
+    })
+})
+
+app.listen(process.env.PORT || 8081)
